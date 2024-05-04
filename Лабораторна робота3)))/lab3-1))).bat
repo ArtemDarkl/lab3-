a26601afde94bead0632b2e6119927dc0f1f0ee6
ArtemDarkl <ostapenkoartem@ukr.net>
2024-05-04 08:32:15 +0300

    Update and rename do_part1.bat to lab3-1))).bat

--- Лабораторна робота3)))/lab3-1))).bat
+++ Лабораторна робота3)))/lab3-1))).bat
@@ -3,4 +3,5 @@ mkdir ПЗ-23-2\Остапенко.А.А\batch\СкрытаяПапка ПЗ-23-
 attrib +h ПЗ-23-2\Остапенко.А.А\batch\СкрытаяПапка
 xcopy /? > ПЗ-23-2\Остапенко.А.А\batch\НеСкрытаяПапка\copyhelp.txt
 xcopy /f /a ПЗ-23-2\Остапенко.А.А\batch\НеСкрытаяПапка\copyhelp.txt ПЗ-23-2\Остапенко.А.А\batch\СкрытаяПапка\copyhelp.txt
-ren /y ПЗ-23-2\Остапенко.А.А\batch\СкрытаяПапка\copyhelp.txt copied_copyhelp.txt
+ren "ПЗ-23-2\Остапенко.А.А\batch\СкрытаяПапка\copyhelp.txt" copied_copyhelp.txt
+pause >nul
